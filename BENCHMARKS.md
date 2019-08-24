cef174c9bc59fb91d6c880851e8944b66716f3fe
Peter Hurford <peter@peterhurford.com>
2019-08-23 19:25:55 -0700

    Add frequency encoding

--- BENCHMARKS.md
+++ BENCHMARKS.md
@@ -1,6 +1,9 @@
 * LR w/ Label Encoding - CV 0.7168 LB ?
+* LGB w/ Frequency Encoding - CV 0.7795 LB ?
 * LGB w/ Label Encoding - CV 0.7813 LB 0.78519 
-* LGB w/ LR encoding - CV 0.8024 LB ?
+* LGB w/ LGB Encoding - CV 0.7954 LB ?
+* LGB w/ LR encoding - CV 0.7999 LB ?
+* LGB w/ LR encoding + All LR - CV 0.8024 LB ?
 * DataRobot 100% AutoML (ENET w/Binning) - CV 0.8031 LB 0.80796
 * LR w/ OHE - CV 0.8032 LB 0.80739
 * LR w/ OHE (fix max_iter) - CV 0.8033 LB ?
--- kernel.py
+++ kernel.py
@@ -1,3 +1,11 @@
+RUN_LR_W_LABEL = False
+RUN_LGB_W_LGB = False
+RUN_LGB_W_FREQ = True
+RUN_LR_WITH_OHE = False
+ADD_LR = False
+RUN_LGB_WITH_LR_ENCODING = False
+
+
 import numpy as np
 import pandas as pd
 
@@ -30,34 +38,9 @@ for c in train.columns:
     test[c] = le.transform(test[c])
 
 
-print_step('OHE')
-train_ohe, test_ohe = ohe(train, test)
-print(train_ohe.shape)
-print(test_ohe.shape)
-
-
 lr_params = {'solver': 'lbfgs', 'C': 0.1151, 'max_iter': 500}
-results_lr = run_cv_model(train_ohe, test_ohe, target, runLR, lr_params, auc, 'lr-ohe')
-train.loc[:, 'lr'] = results_lr['train']
-test.loc[:, 'lr'] = results_lr['test']
-
-
-for col in [c for c in train.columns if 'bin' not in c and 'lr' not in c and 'ord_0' not in c]:
-    print('Encoding {}'.format(col))
-    tr = pd.DataFrame(train[col])
-    te = pd.DataFrame(test[col])
-    tr, te = ohe(tr, te)
-    print(tr.shape)
-    print(te.shape)
-    col_encode = run_cv_model(tr, te, target, runLR, lr_params, auc, 'lr-{}'.format(col))
-    train.loc[:, 'lr_{}'.format(col)] = col_encode['train']
-    test.loc[:, 'lr_{}'.format(col)] = col_encode['test']
-    train.drop(col, axis=1, inplace=True)
-    test.drop(col, axis=1, inplace=True)
-
-
-print_step('Glimpsing')
-print(train.head())
+if RUN_LR_W_LABEL:
+    results = run_cv_model(train, test, target, runLR, lr_params, auc, 'lr-label')
 
 
 lgb_params = {'application': 'binary',
@@ -78,7 +61,63 @@ lgb_params = {'application': 'binary',
               'early_stop': 100,
               'verbose_eval': 50,
               'num_rounds': 10000}
-results = run_cv_model(train, test, target, runLGB, lgb_params, auc, 'lgb')
+if RUN_LGB_W_LABEL:
+    results = run_cv_model(train, test, target, runLGB, lgb_params, auc, 'lgb-label')
+
+
+cat_cols =  [c for c in train.columns if 'bin' not in c and 'lr' not in c and 'ord_0' not in c] 
+if RUN_LGB_W_LGB:
+    for col in cat_cols:
+        train[col] = train[col].astype('category')
+        test[col] = test[col].astype('category')
+    lgb_params2 = lgb_params.copy()
+    lgb_params2['cat_cols'] = cat_cols
+    results = run_cv_model(train, test, target, runLGB, lgb_params2, auc, 'lgb-lgb')
+
+
+if RUN_LGB_W_FREQ:
+    traintest = pd.concat([train, test])
+    for col in cat_cols:
+        print('Frequency Encoding {}'.format(col))
+        traintest.loc[:, 'freq_{}'.format(col)] = traintest.groupby(col)[col].transform('count')
+        traintest.drop(col, axis=1, inplace=True)
+    train = traintest.iloc[:train.shape[0], :]
+    test = traintest.iloc[train.shape[0]:, :]
+    print(train.shape)
+    print(test.shape)
+    results = run_cv_model(train, test, target, runLGB, lgb_params, auc, 'lgb-freq')
+
+
+print_step('OHE')
+train_ohe, test_ohe = ohe(train, test)
+print(train_ohe.shape)
+print(test_ohe.shape)
+
+
+if RUN_LR_WITH_OHE:
+    results_lr = run_cv_model(train_ohe, test_ohe, target, runLR, lr_params, auc, 'lr-ohe')
+
+
+if ADD_LR:
+    train.loc[:, 'lr'] = results_lr['train']
+    test.loc[:, 'lr'] = results_lr['test']
+
+
+if RUN_LGB_WITH_LR_ENCODING:
+    for col in cat_cols:
+        print('LR Encoding {}'.format(col))
+        tr = pd.DataFrame(train[col])
+        te = pd.DataFrame(test[col])
+        tr, te = ohe(tr, te)
+        print(tr.shape)
+        print(te.shape)
+        col_encode = run_cv_model(tr, te, target, runLR, lr_params, auc, 'lr-{}'.format(col))
+        train.loc[:, 'lr_{}'.format(col)] = col_encode['train']
+        test.loc[:, 'lr_{}'.format(col)] = col_encode['test']
+        train.drop(col, axis=1, inplace=True)
+        test.drop(col, axis=1, inplace=True)
+
+    results = run_cv_model(train, test, target, runLGB, lgb_params, auc, 'lgb-lr')
 
 print_step('Feature importance')
 imports = results['importance'].groupby('feature')['feature', 'importance'].mean().reset_index()
